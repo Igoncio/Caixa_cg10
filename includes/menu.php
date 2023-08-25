62c6ca38fd9676fb9050510e0451adcadd3153b0
Igor <igoralbieri04@gmail.com>
2023-08-24 22:49:40 -0400

    a

---
+++ includes/menu.php
@@ -0,0 +1,24 @@
+<?php
+ 
+ ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/menu.css">
+    <title>Menu</title>
+</head>
+<body>
+
+    <header class="hero">
+        <nav>
+            <img  id="logo1" src="../imgs/logo separada 40 anos.png" alt="">
+            <ul>
+                <li><a href="#">Cadastro</a></li>
+                <li><a href="#">Caixa</a></li>
+                <li><a href="#">Produtos</a></li>
+            </ul>
+            <img    id="logo2"  src="../imgs/cg10logo-depositphotos-bgremover.png" alt="">
+        </nav>
+    </header>
