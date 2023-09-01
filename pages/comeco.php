cc99ffbc42fa6e8e65fea323bc36642b5f103e7b
Igor <igoralbieri04@gmail.com>
2023-08-31 21:00:55 -0400

    a

--- pages/comeco.php
+++ pages/comeco.php
@@ -2,6 +2,7 @@
  
  include_once('../includes/menu.php');
 
+
 ?>
 
 <!DOCTYPE html>
@@ -13,9 +14,56 @@
     <title>Caixa</title>
 </head>
 <body>
-    
 
-  
+
+<button onclick="soma()">Fanta</button>
+    <button onclick="soma()">Vodka</button>
+
+    <button onclick="soma()">Tang</button>
+
+    <button onclick="soma()">suco</button>
+    <p id="fantaEscolha"></p>
+    <p id="vodkaEscolha"></p>
+    <p id="vodkaEscolha"></p>
+    <p id="vodkaEscolha"></p>
+
+</body>
+
+<script>
+
+var total = 0
+var fanta = 0
+var vodka = 0
+var tang = 0
+var suco = 0
+
+const vodkaEscolha = document.getElementById("vodkaEscolha")
+function soma(preco,escolha){
+
+    total += preco
+    console.log(total)
+    if(escolha == 1){
+
+        fanta = fanta + 1
+
+    }
+    if(escolha == 2){
+        vodka += 1
+    }
+    if(escolha == 3){
+        tang += 1
+    }
+    if(escolha == 4){
+        suco += 1
+    }
+
+    console.log("fanta "+ fanta)
+    console.log("vodka "+ vodka)
+    console.log("tang "+ tang)
+    console.log("suco "+ suco)
+    vodkaEscolha.innerHTML = "Vodka = "+vodka 
+
+}
 
 </body>
 </html>
